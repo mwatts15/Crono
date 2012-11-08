e47e19f4cd2029bb322e908f41e1ad41a18859fc
Troy Varney <troy.varney@att.net>
2012-11-07 19:15:39 -0600

    Changed prelude.lisp to use the correct syntax for defines.

--- crono/prelude.lisp
+++ crono/prelude.lisp
@@ -1,103 +1,63 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % boolean functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-(define not (bool)
-  (if (= bool nil)
-    t
-    nil))
+(define not (\ (bool) (if (= bool nil) #t #f)))
 
-% TODO: Rewrite these to take an arbitrary number of arguments.
-(define and (a b)
-  (if a
-    b
-    nil))
+(define and (\ (a b) (if a b nil)))
 
-(define or (a b)
-  (if a
-    t
-    b))
-% /boolean
+(define or (\ (a b) (if a t b)))
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % higher order functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-(define map (f l)
-  (if (= l nil)
-    nil
-    (cons (f (car l)) (map f (cdr l)))))
+(define map (\ (f l) (if (= l nil) nil (cons (f (car l)) (map f (cdr l))))))
 
-(define _foldl (f l s)
-  (if (= l nil)
-    s
-    (_foldl f (cdr l) (f s (car l)))))
+(define _foldl (\ (f l s) (if (= l nil) s (_foldl f (cdr l) (f s (car l))))))
 
-(define foldl (f l)
-  (if (= l nil)
-    nil
-    (_foldl f (cdr l) (car l))))
+(define foldl (\ (f l) (if (= l nil) nil (_foldl f (cdr l) (car l)))))
 
-(define _foldr (f l s)
-  (if (= l nil)
-    s
-    (f (car l) (_foldr f (cdr l) s))))
+(define _foldr (\ (f l s) (if (= l nil) s (f (car l) (_foldr f (cdr l) s)))))
 
-(define foldr (f l)
-  (if (= l nil)
-    nil
-    (_foldr f (cdr l) (car l))))
-%/higher order
+(define foldr (\ (f l) (if (= l nil) nil (_foldr f (cdr l) (car l)))))
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % math functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-(define ! (n)
-  (if (= n 0)
-    1
-    (* (! (- n 1)) n)))
+(define ! (\ (n) (if (= n 0) 1 (* (! (- n 1)) n))))
 
-(define mod (x y)
-  (if (< x y)
-    x
-    (mod (- x y) y)))
-% /math
-
+(define mod (\ (x y) (if (< x y) x (mod (- x y) y))))
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % combinators
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %identity combinator
-(define <I> (x)
-  x)
+(define <I> (\ (x) x))
   
 %Constant combinator
-(define <K> (x y)
-  x)
+(define <K> (\ (x y) x)
 
 %General application combinator
-(define <S> (f g x)
-  (f x (g x)) )
+(define <S> (\ (f g x) (f x (g x))))
   
 %Function composition combinator
-(define <B> (f g x)
-  (f (g x)) )
+(define <B> (\ (f g x) (f (g x))))
 
 %
-(define <C> (f x y)
-  (f y x) )
+(define <C> (\ (f x y) (f y x)))
 
 %(define <Y> (f)
 %  (f (<Y> f)) )
 %Cannata's incorrect definition of Y combinator
 
 %Recursive function 
-(define <Z> (f)
+(define <Z> (\ (f)
     ((\ (x) (f (\ (v) ((x x) v))))
-     (\ (x) (f (\ (v) ((x x) v))))))
+     (\ (x) (f (\ (v) ((x x) v)))))))
 
-(define <Y> (f)
-  (\ (x) (f (x x))) (\ (x) (f (x x))) )
+(define <Y> (\ (f)
+  (\ (x) (f (x x))) (\ (x) (f (x x))) ))
 
 
   
-(define <COND> (p f g x)
-  (if (p x) (f x) (g x)) )
+(define <COND> (\ (p f g x)
+  (if (p x) (f x) (g x))))
