ab70bb001cd81f7d6bd7dcd5f4ef61fffb5494d4
Mark Watts <markw@cs.utexas.edu>
2012-11-14 19:11:54 -0600

    Whitespace

--- crono/src/crono/type/TypeId.java
+++ crono/src/crono/type/TypeId.java
@@ -2,46 +2,46 @@ package crono.type;
 
 public class TypeId extends Atom {
     public static final TypeId TYPEID = new TypeId(":type-id", TypeId.class,
-						   Atom.TYPEID);
-    
+            Atom.TYPEID);
+
     public final String image;
     public final Class type;
-    
+
     public final TypeId parent;
-    
+
     public TypeId(String image, Class type) {
-	this(image, type, null);
+        this(image, type, null);
     }
     public TypeId(String image, Class type, TypeId parent) {
-	this.type = type;
-	this.image = image;
-	this.parent = parent;
+        this.type = type;
+        this.image = image;
+        this.parent = parent;
     }
-    
+
     public boolean isType(CronoType object) {
-	TypeId id = object.typeId();
-	while(id != null) {
-	    if(this.equals(id)) {
-		return true;
-	    }
-	    id = id.parent;
-	}
-	return false;
+        TypeId id = object.typeId();
+        while(id != null) {
+            if(this.equals(id)) {
+                return true;
+            }
+            id = id.parent;
+        }
+        return false;
     }
-    
+
     public boolean equals(Object o) {
-	return ((o instanceof TypeId) &&
-		type.equals(((TypeId)o).type) &&
-		image.equals(((TypeId)o).image));
+        return ((o instanceof TypeId) &&
+                type.equals(((TypeId)o).type) &&
+                image.equals(((TypeId)o).image));
     }
-    
+
     public TypeId typeId() {
-	return TypeId.TYPEID;
+        return TypeId.TYPEID;
     }
     public String toString() {
-	if(isQuoted()) {
-	    return "'" + image;
-	}
-	return image;
+        if(isQuoted()) {
+            return "'" + image;
+        }
+        return image;
     }
 }
