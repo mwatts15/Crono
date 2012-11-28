daa105edf01e4babb3100984b57c24bfb03576a4
Troy Varney <troy.varney@att.net>
2012-11-27 18:47:50 -0600

    Changed prelude to use recursive defun function for recursive functions.

--- crono/prelude.lisp
+++ crono/prelude.lisp
@@ -10,22 +10,22 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % higher order functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-(define map (\ (f l) (if (= l nil) nil (cons (f (car l)) (map f (cdr l))))))
+(defun map (f l) (if (= l nil) nil (cons (f (car l)) (map f (cdr l)))))
 
-(define _foldl (\ (f l s) (if (= l nil) s (_foldl f (cdr l) (f s (car l))))))
+(defun _foldl (f l s) (if (= l nil) s (_foldl f (cdr l) (f s (car l)))))
 
-(define foldl (\ (f l) (if (= l nil) nil (_foldl f (cdr l) (car l)))))
+(defun foldl (f l) (if (= l nil) nil (_foldl f (cdr l) (car l))))
 
-(define _foldr (\ (f l s) (if (= l nil) s (f (car l) (_foldr f (cdr l) s)))))
+(defun _foldr (f l s) (if (= l nil) s (f (car l) (_foldr f (cdr l) s))))
 
-(define foldr (\ (f l) (if (= l nil) nil (_foldr f (cdr l) (car l)))))
+(defun foldr (\ (f l) (if (= l nil) nil (_foldr f (cdr l) (car l)))))
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % math functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-(define ! (\ (n) (if (= n 0) 1 (* (! (- n 1)) n))))
+(defun ! (n) (if (= n 0) 1 (* (! (- n 1)) n)))
 
-(define mod (\ (x y) (if (< x y) x (mod (- x y) y))))
+(defun mod (x y) (if (< x y) x (mod (- x y) y)))
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % combinators
